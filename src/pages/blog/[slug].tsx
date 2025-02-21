43d520c26880a8d04c7aad78937b7754be31e418
Mikka <i@cvyl.me>
2025-02-21 05:32:28 +0100

    feat: simplify useHead function for blog post metadata management

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -103,50 +103,30 @@ export default defineComponent({
 					}
 					postHTML.value = await marked(contentWithoutFrontmatter)
 					updateTOC()
-          watch([title, description, cover, route.params.slug, tags], () => {
-            if (title.value && route.params.slug) {
-              useHead({
-                title: title.value,
-                link: [
-                  { rel: 'canonical', href: `https://cvyl.me/blog/${route.params.slug}` }
-                ],
-                meta: [
-                  { name: 'description', content: description.value || '' },
-                  { property: 'og:url', content: `https://cvyl.me/blog/${route.params.slug}` },
-                  { property: 'og:type', content: 'article' },
-                  { property: 'og:title', content: title.value },
-                  { property: 'og:site_name', content: "cvyl's Blog" },
-                  { property: 'og:description', content: description.value || '' },
-                  { property: 'og:locale', content: 'en_US' },
-                  { property: 'og:image', content: cover.value || '' },
-                  { property: 'article:author', content: 'cvyl' },
-                  { property: 'twitter:title', content: title.value },
-                  { property: 'twitter:description', content: description.value || '' },
-                  { property: 'twitter:card', content: 'summary_large_image' },
-                  { property: 'twitter:image', content: cover.value || '' },
-                  { property: 'twitter:site', content: 'https://cvyl.me' },
-                  ...tags.value.map((tag) => ({
-                    property: 'article:tag',
-                    content: tag
-                  }))
-                ].filter(meta => meta.content) // Remove empty meta tags
-              })
-            }
-            else {
-              useHead({
-                title: 'cvyl\'s Blog',
-                link: [{ rel: 'canonical', href: 'https://cvyl.me/blog' }],
-                meta: [
-                  { name: 'description', content: 'cvyl 的个人博客' },
-                  { property: 'og:url', content: 'https://cvyl.me/blog' },
-                  { property: 'og:title', content: 'cvyl\'s Blog' },
-                  { property: 'og:description', content: 'cvyl 的个人博客' },
-                  { property: 'twitter:title', content: 'cvyl\'s Blog' },
-                  { property: 'twitter:description', content: 'cvyl 的个人博客' }
-                ]
-              })
-            }
-          }, { immediate: true })
+          useHead({
+            title: title.value,
+            link: [{ rel: 'canonical', href: `https://cvyl.me/blog/${route.params.slug}` }],
+            meta: [
+              { name: 'description', content: description.value || '' },
+              { property: 'og:url', content: `https://cvyl.me/blog/${route.params.slug}` },
+              { property: 'og:type', content: 'article' },
+              { property: 'og:title', content: title.value },
+              { property: 'og:site_name', content: "cvyl's Blog" },
+              { property: 'og:description', content: description.value || '' },
+              { property: 'og:locale', content: 'en_US' },
+              { property: 'og:image', content: cover.value || '' },
+              { property: 'article:author', content: 'cvyl' },
+              { property: 'twitter:title', content: title.value },
+              { property: 'twitter:description', content: description.value || '' },
+              { property: 'twitter:card', content: 'summary_large_image' },
+              { property: 'twitter:image', content: cover.value || '' },
+              { property: 'twitter:site', content: 'https://cvyl.me' },
+              ...tags.value.map((tag) => ({
+                property: 'article:tag',
+                content: tag
+              }))
+            ]
+          })
 				} catch (error) {
 					router.push('/')
 				}
